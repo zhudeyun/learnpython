f5a881695b0773b032fbf7c18c18d23acc94ddcc
zhudeyun <publicregister01@163.com>
2018-07-27 15:21:40 +0800

    add exercise01..

---
+++ exercise01.py
@@ -0,0 +1,6 @@
+age=input("please input age:")
+print(int(age))
+if int(age)>18:
+    print("adult")
+else:
+    print("teenage")
---
+++ integer.py
@@ -0,0 +1,7 @@
+#print absolute value of integer
+a=input("input a =")
+a=int(a)
+if a>0:
+	print(a)
+else:
+	print(-a)
